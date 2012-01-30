6dcaf0dbda8e53457ef98faf17256d297b98d922
Jerad Bitner <sirkitree@gmail.com>
2012-01-29 21:47:11 -0700

    Make it funny without being too annoying.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // npm install jerk twss
 // node server.js
 var jerk = require("jerk")
-  , twss = require("twss");
+  , twss = require("twss")
+  , count = 0;
 
 var options = {
   server: 'irc.freenode.net'
@@ -13,10 +14,16 @@ twss.threshold = 0.5;
 
 jerk(function(j) {
   j.watch_for(' ', function(message) {
-    console.log(message.text[0]);
+    // console.log(message.text[0]);
     var text = message.text[0];
     if (twss.is(text)) {
-      message.say(text + "... That's what she said!");
+      // In order to keep the threshold low enough to be funny, and yet not be
+      // completly annoying, we only report every 20 times a message qualifies.
+      if (count == 20) {
+        message.say(text + "... That's what she said!");
+        count = 1;
+      }
+      count++;
     }
   });
 
